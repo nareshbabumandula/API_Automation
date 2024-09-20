2e73cbb5e3b129cebb0185da22191929e95dcfe3
nareshbabumandula <naresh223@gmail.com>
2024-09-20 09:02:40 +0530

    JSON Array

--- RestAssured_Automation/src/test/java/stepdefinitions/RestfulAPISteps.java
+++ RestAssured_Automation/src/test/java/stepdefinitions/RestfulAPISteps.java
@@ -12,7 +12,7 @@ import io.restassured.response.Response;
 
 public class RestfulAPISteps {
 	Response response;
-	
+
 	@When("I perform a GET operation for objects")
 	public void i_perform_a_get_operation_for_objects() {
 		response =given()
@@ -20,17 +20,24 @@ public class RestfulAPISteps {
 				.get("https://api.restful-api.dev/objects")
 				.then()
 				.extract().response();
-				
+
 		JsonPath jsonResponse = new JsonPath(response.asString());
 		int ID = jsonResponse.getInt("[0].id");
 		String name = jsonResponse.getString("[0].name");
 		String color = jsonResponse.getString("[0].data.color");
-		
+
 		System.out.println(ID);
 		System.out.println(name);
 		System.out.println(color);
+
+		//get values of JSON array after getting array size
+		int s = jsonResponse.getInt("[0].size()");
+		for(int i = 0; i < s; i++) {
+			int id = jsonResponse.getInt("[i].id");
+			System.out.println(id);
+		}
 	}
-	
+
 	@Then("I should see status code as {int} in the response")
 	public void verifyUserResponse(int statusCode) {
 		assertEquals(statusCode, response.getStatusCode());
